732adbec4c667c20a87f5583a851eec030acd8f7
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2023-10-14 00:48:09 +0700

    fix cors

--- server/src/app.js
+++ server/src/app.js
@@ -28,6 +28,19 @@ app.use(cors({
     origin: 'https://api-bo.onrender.com'
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://103.57.220.42:3000');
+    // Các tiêu đề CORS khác nếu cần
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use("/api/setup", walletSys)
 
 app.use("/api/users", userRouter)
