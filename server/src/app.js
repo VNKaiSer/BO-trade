6d26568b459329f8bf47b1886e3947b703e6e032
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2023-10-14 00:48:39 +0700

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -29,7 +29,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://103.57.220.42:3000');
+    res.header('Access-Control-Allow-Origin', 'https://api-bo.onrender.com');
     // Các tiêu đề CORS khác nếu cần
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
